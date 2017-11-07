746dcaa2c0c080196eb0a24bf2283795b9291b82
Noixas <acmrz@outlook.com>
2017-11-07 10:12:46 +0100

    Comments...

--- Component.java
+++ Component.java
@@ -1,9 +1,10 @@
+//TODO:Make component an interface
+//Create Physics, input and Graphics component
 class PhysicsComponent
 {
 public  void update(GameObject pGObject, World pWorld)
   {
     //pGObject.x += pGObject.velocity;
-    //pWorld.resolveCollision( bjorn.x, bjorn.y, bjorn.velocity);
   }
 
 };
